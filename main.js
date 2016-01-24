a6a41c19f0a1c5c808f3c99c01bcd6d186bf845b
Otto Seiskari <otto.seiskari@gmail.com>
2016-01-24 18:12:39 +0200

    datepicker

--- main.js
+++ main.js
@@ -2,11 +2,33 @@ var FIREBASE_URL = "https://shining-fire-655.firebaseio.com";
 
 // adapted from the BackboneFire example app
 
-var Day = Backbone.Model.extend({
+/*var Day = Backbone.Model.extend({
     defaults: { minutes: 0 }
+});*/
+
+var Day = Backbone.Firebase.Model.extend({
+  urlRoot: FIREBASE_URL + '/days'
 });
 
-var DayCollection = Backbone.Firebase.Collection.extend({
+var DayEditView = Backbone.View.extend({
+    events: {
+        "click #save-day" : "saveDay",
+    },
+    initialize: function() {
+        this.input = this.$("#minute-input");
+        this.listenTo(this.model, 'change', this.render);
+        this.render();
+    },
+    render: function() {
+        this.input.val(this.model.toJSON().minutes);
+        return this;
+    },
+    saveDay: function() {
+        this.model.set({minutes: this.input.val()});
+    }
+});
+
+/*var DayCollection = Backbone.Firebase.Collection.extend({
   model: Day,
   url: FIREBASE_URL
 });
@@ -42,8 +64,33 @@ var MainView = Backbone.View.extend({
         this.input.val('');
     }
 });
+*/
 
 $(function() {
-    var collection = new DayCollection();
-    var app = new MainView({ collection: collection });
+    //var collection = new DayCollection();
+    //var app = new MainView({ collection: collection });
+
+    var dp = $('#datetimepicker').datetimepicker({
+        inline: true,
+        format: 'YYYY-MM-DD'
+    });
+
+    var editor_container = $('#date-edit');
+    var editor_template = editor_container.html();
+    var editor_view = null;
+
+    dp.on('dp.change', function (e) {
+        var iso_date = e.date.format('YYYY-MM-DD');
+
+        if (editor_view !== null) editor_view.remove();
+        editor_container.html('');
+        var el = $('<div/>');
+        editor_container.append(el);
+        el.html(editor_template);
+
+        editor_view = new DayEditView({
+            el: el,
+            model: new Day({id: iso_date})
+        });
+    });
 });
