ee5de572c5df05759fcde8c6a4dcfe3722f02d74
Otto Seiskari <otto.seiskari@gmail.com>
2016-01-24 19:07:01 +0200

    added total

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 var FIREBASE_URL = "https://shining-fire-655.firebaseio.com";
-
-// adapted from the BackboneFire example app
-
-/*var Day = Backbone.Model.extend({
-    defaults: { minutes: 0 }
-});*/
+var DAYS_ROOT = FIREBASE_URL + '/days';
 
 var Day = Backbone.Firebase.Model.extend({
-  urlRoot: FIREBASE_URL + '/days'
+  urlRoot: DAYS_ROOT,
+  yankeeDay: function() {
+      return moment(this.id).format('MM/DD/YYYY');
+  }
 });
 
 var DayEditView = Backbone.View.extend({
@@ -28,9 +26,48 @@ var DayEditView = Backbone.View.extend({
     }
 });
 
-/*var DayCollection = Backbone.Firebase.Collection.extend({
+var EditorView = Backbone.View.extend({
+    initialize: function() {
+        this.template = this.$el.html();
+        this.day_view = null;
+    },
+    changeDate: function(iso_date) {
+        if (this.day_view !== null) this.day_view.remove();
+        this.$el.html('');
+        var new_el = $('<div/>');
+        this.$el.append(new_el);
+        new_el.html(this.template);
+        this.day_view = new DayEditView({
+            el: new_el,
+            model: new Day({id: iso_date})
+        });
+    }
+});
+
+var TotalView = Backbone.View.extend({
+    initialize: function() {
+        var that = this;
+        this.collection.on('all', function() {
+            that.render();
+        });
+    },
+    render: function() {
+        var total = this.collection.totalMinutes();
+        this.$el.html(''+total);
+    }
+});
+
+var DayCollection = Backbone.Firebase.Collection.extend({
   model: Day,
-  url: FIREBASE_URL
+  url: DAYS_ROOT,
+
+  totalMinutes: function() {
+      var sum = 0;
+      this.each(function (day) {
+          sum += parseInt(day.attributes.minutes);
+      });
+      return sum;
+  }
 });
 
 var DayView = Backbone.View.extend({
@@ -39,58 +76,51 @@ var DayView = Backbone.View.extend({
         this.listenTo(this.model, "change", this.render);
     },
     render: function() {
-        this.$el.html(this.model.toJSON().minutes);
+        this.$el.html(this.model.minutes);
         return this;
     },
 });
 
 var MainView = Backbone.View.extend({
     el: $('#app-container'),
-    events: {
-        "click #save-day" : "saveDay",
-    },
     initialize: function() {
-        this.list = this.$("#day-list");
-        this.input = this.$("#minute-input");
-        this.listenTo(this.collection, 'add', this.addOne);
+        this.calendar = this.$("#datetimepicker");
+        this.listenTo(this.collection, 'add', this.renderOne);
+
+        var dp = this.calendar.datetimepicker({
+            inline: true,
+            format: 'YYYY-MM-DD'
+        });
+
+        this.editor = new EditorView({el: this.$('#date-edit')});
+        this.total_view = new TotalView({
+            el: this.$('#total'),
+            collection: this.collection
+        });
+        var that = this;
+
+        dp.on('dp.change', function(e) {
+            var iso_date = e.date.format('YYYY-MM-DD');
+            that.editor.changeDate(iso_date);
+            that.renderAll();
+        });
+
+        dp.on('dp.update', function() { that.renderAll(); });
     },
-    addOne: function(day) {
-        var view = new DayView({model: day});
-        this.list.append(view.render().el);
+    renderAll: function() {
+        this.$el.find('.day').attr('style','');
+        var that = this;
+    	this.collection.each(function(day){
+            that.renderOne(day);
+    	});
     },
-    saveDay: function(e) {
-        if (!this.input.val()) { return; }
-        this.collection.create({minutes: this.input.val()});
-        this.input.val('');
+    renderOne: function(day) {
+        var el = this.calendar.find('[data-day="'+day.yankeeDay()+'"]');
+        el.attr('style', 'font-weight: bold; color: #008000');
     }
 });
-*/
 
 $(function() {
-    //var collection = new DayCollection();
-    //var app = new MainView({ collection: collection });
-
-    var dp = $('#datetimepicker').datetimepicker({
-        inline: true,
-        format: 'YYYY-MM-DD'
-    });
-
-    var editor_container = $('#date-edit');
-    var editor_template = editor_container.html();
-    var editor_view = null;
-
-    dp.on('dp.change', function (e) {
-        var iso_date = e.date.format('YYYY-MM-DD');
-
-        if (editor_view !== null) editor_view.remove();
-        editor_container.html('');
-        var el = $('<div/>');
-        editor_container.append(el);
-        el.html(editor_template);
-
-        editor_view = new DayEditView({
-            el: el,
-            model: new Day({id: iso_date})
-        });
-    });
+    var collection = new DayCollection();
+    var app = new MainView({ collection: collection });
 });
